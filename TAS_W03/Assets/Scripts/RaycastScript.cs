12083091990e5608b3a219b7a75f21e6a736fad2
Unknown <kaa516@nyu.edu>
2019-02-20 10:43:41 -0500

    wk 3 commit

---
+++ TAS_W03/Assets/Scripts/RaycastScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    public Camera camera;
+
+    public Material otherMaterial;
+
+    public ArrayList objsOfInt;
+    public ThirdPersonCameraController thirdPovScriipt;
+
+    Vector3 mousePosition;
+    public float angle;
+    public HingeJoint hj;
+
+    public bool objectIsHit;
+    public float zoomOutSpeed;
+
+    public Vector3 targetPos;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //camera = GetComponent<Camera>();
+
+        objectIsHit = false;
+        targetPos = new Vector3(0, 0, -3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "ObjectOfInterest")
+            {
+                Debug.Log("objHit!");
+
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material = otherMaterial;
+              
+                //transform.Translate(new Vector3(0, 20, 0));
+            }
+        }
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);      
+
+        //Zooming Out
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView <= 125)
+                Camera.main.fieldOfView += 2;
+            if (Camera.main.orthographicSize <= 20)
+                Camera.main.orthographicSize += 0.5f;
+
+        }
+        //Zooming In
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 2)
+                Camera.main.fieldOfView -= 2;
+            if (Camera.main.orthographicSize >= 11)
+                Camera.main.orthographicSize -= 0.5f;
+        }
+
+    }
+
+    void avoidObj(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            hitColliders[i].SendMessage("sensingObj");
+            i++;
+            //transform.position = Mathf.MoveTowards(targetPos, 1, Time.deltaTime * 3);
+
+        }
+    }
+}
---
+++ TAS_W03/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour {
+
+    #region Internal References
+    private Transform _app;
+    private Transform _view;
+    private Transform _cameraBaseTransform;
+    private Transform _cameraTransform;
+    public Transform _cameraLookTarget;
+    private Transform _avatarTransform;
+    private Rigidbody _avatarRigidbody;
+    #endregion
+
+    #region Public Tuning Variables
+    public Vector3 avatarObservationOffset_Base;
+    public float followDistance_Base;
+    public float verticalOffset_Base;
+    public float pitchGreaterLimit;
+    public float pitchLowerLimit;
+    public float fovAtUp;
+    public float fovAtDown;
+    #endregion
+
+    #region Persistent Outputs
+    //Positions
+    private Vector3 _camRelativePostion_Auto;
+
+    //Directions
+    private Vector3 _avatarLookForward;
+
+    //Scalars
+    private float _followDistance_Applied;
+    private float _verticalOffset_Applied;
+    #endregion
+
+    private void Awake()
+    {
+        _app = GameObject.Find("Application").transform;
+        _view = _app.Find("View");
+        _cameraBaseTransform = _view.Find("CameraBase");
+        _cameraTransform = _cameraBaseTransform.Find("Camera");
+        _cameraLookTarget = _cameraBaseTransform.Find("CameraLookTarget");
+
+        _avatarTransform = _view.Find("AIThirdPersonController");
+        _avatarRigidbody = _avatarTransform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+            _ManualUpdate();
+        else
+            _AutoUpdate();
+    }
+
+    #region States
+    private void _AutoUpdate()
+    {
+        _ComputeData();
+        _FollowAvatar();
+        _LookAtAvatar();
+    }
+    private void _ManualUpdate()
+    {
+        _FollowAvatar();
+        _ManualControl();
+    }
+    #endregion
+
+    #region Internal Logic
+
+    float _standingToWalkingSlider = 0;
+
+    private void _ComputeData()
+    {
+        _avatarLookForward = Vector3.Normalize(Vector3.Scale(_avatarTransform.forward, new Vector3(1, 0, 1)));
+
+        if (_Helper_IsWalking())
+        {
+            _standingToWalkingSlider = Mathf.MoveTowards(_standingToWalkingSlider, 1, Time.deltaTime * 3);
+        }
+        else
+        {
+            _standingToWalkingSlider = Mathf.MoveTowards(_standingToWalkingSlider, 0, Time.deltaTime);
+        }
+
+        float _followDistance_Walking = followDistance_Base;
+        float _followDistance_Standing = followDistance_Base * 2;
+
+        float _verticalOffset_Walking = verticalOffset_Base;
+        float _verticalOffset_Standing = verticalOffset_Base * 4;
+
+        _followDistance_Applied = Mathf.Lerp(_followDistance_Standing, _followDistance_Walking, _standingToWalkingSlider);
+        _verticalOffset_Applied = Mathf.Lerp(_verticalOffset_Standing, _verticalOffset_Walking, _standingToWalkingSlider);
+    }
+
+    private void _FollowAvatar()
+    {
+        _camRelativePostion_Auto = _avatarTransform.position;
+
+        _cameraLookTarget.position = _avatarTransform.position + avatarObservationOffset_Base;
+        _cameraTransform.position = _avatarTransform.position - _avatarLookForward * _followDistance_Applied + Vector3.up * _verticalOffset_Applied;
+    }
+
+    private void _LookAtAvatar()
+    {
+        _cameraTransform.LookAt(_cameraLookTarget);
+    }
+
+    private void _ManualControl()
+    {
+        Vector3 _camEulerHold = _cameraTransform.localEulerAngles;
+
+        if (Input.GetAxis("Mouse X") != 0)
+            _camEulerHold.y += Input.GetAxis("Mouse X");
+
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            float temp = _camEulerHold.x - Input.GetAxis("Mouse Y");
+            temp = (temp + 360) % 360;
+
+            if (temp < 180)
+                temp = Mathf.Clamp(temp, 0, 80);
+            else
+                temp = Mathf.Clamp(temp, 360 - 80, 360);
+
+            _camEulerHold.x = temp;
+        }
+
+        Debug.Log("The V3 to be applied is " + _camEulerHold);
+        _cameraTransform.localRotation = Quaternion.Euler(_camEulerHold);
+    }
+    #endregion
+
+    #region Helper Functions
+
+    private Vector3 _lastPos;
+    private Vector3 _currentPos;
+    private bool _Helper_IsWalking()
+    //private bool Helper_IsThere00I()
+    {
+        _lastPos = _currentPos;
+        _currentPos = _avatarTransform.position;
+        float velInst = Vector3.Distance(_lastPos, _currentPos) / Time.deltaTime;
+
+        if (velInst > .15f)
+            return true;
+        else return false; 
+    }
+
+    #endregion
+}
+
+public enum CameraStates { Manuel, Automatic, Idle }
+
+//Physics.OverlapSphere(_avatarTransform.position, 5);
+//return false;
